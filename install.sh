51dde45b19c124a7965d58e7cf463de0b5904019
Vanja Jelic <jelicvanja@yahoo.com>
2015-07-31 22:15:06 +0200

    added current path

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-DOTFILES=$HOME/.dotfiles
+CUR_PATH=$(pwd)
 
 echo "creating dotfiles..."
 linkables=$( ls -1 -d *.symlink )
 for file in $linkables ; do
 	target="$HOME/.$( basename $file ".symlink" )"
 	#echo "creating symlink for $file"
-	echo "copying $file to $target"
-	cp $file $target
+	echo "copying $CUR_PATH/$file to $target"
+	cp $CUR_PATH/$file $target
 done
