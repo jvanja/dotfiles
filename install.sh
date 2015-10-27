6938582d1ee64ec356b6c4b07ac3f655ac639a41
Vanja Jelic <jelicvanja@yahoo.com>
2015-10-27 11:09:37 +0100

    added zshrc

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ echo "creating dotfiles..."
 linkables=$( ls -1 -d *.symlink )
 for file in $linkables ; do
 	target="$HOME/.$( basename $file ".symlink" )"
-	#echo "creating symlink for $file"
 	echo "copying $CUR_PATH/$file to $target"
 	cp $CUR_PATH/$file $target
 done
