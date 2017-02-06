91af66921c46412ce122bf1fe5f6afdae6949d7a
Vanja Jelic <jelicvanja@yahoo.com>
2017-02-06 09:36:59 +0100

    added key repeat

--- macos.sh
+++ macos.sh
@@ -1,2 +1,3 @@
-defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
-defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+# Set a blazingly fast keyboard repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+defaults write NSGlobalDomain InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
