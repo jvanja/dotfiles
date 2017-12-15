d4b44766a6961944bff9950f93de3b6818491575
Vanja Jelic <jelicvanja@yahoo.com>
2017-12-15 22:28:14 +0100

    added update and cleanup

--- brew.sh
+++ brew.sh
@@ -1,48 +1,60 @@
-advancecomp
-apple-gcc42
-automake
-bison@2.7
-boost
-cmake
-homebrew/php/composer
-cscope
-ctags
-dnsmasq
-doxygen
-homebrew/php/drush
-ffmpeg
-flex
-fzf
-gcc@4.9
-git
-imagemagick
-isl@0.11
-libksba
-libvo-aacenc
-libxslt
-mcrypt
-mongodb
-mysql
-neovim
-nvm
-optipng
-pdf2htmlex
-pngquant
-poppler
-python3
-re2c
-reattach-to-user-namespace
-rename
-homebrew/php/robo
-sbt
-telnet
-homebrew/php/terminus
-the_silver_searcher
-tmux
-tree
-utf8proc
-vim
-homebrew/php/wp-cli
-yarn
-zlib
-zsh
+#!/usr/bin/env bash
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# list of tools from 12/15/2017
+brew install advancecomp
+brew install apple-gcc42
+brew install automake
+brew install bison@2.7
+brew install boost
+brew install cmake
+brew install homebrew/php/composer
+brew install cscope
+brew install ctags
+brew install dnsmasq
+brew install doxygen
+brew install homebrew/php/drush
+brew install ffmpeg
+brew install flex
+brew install fzf
+brew install gcc@4.9
+brew install git
+brew install imagemagick
+brew install isl@0.11
+brew install libksba
+brew install libvo-aacenc
+brew install libxslt
+brew install mcrypt
+brew install mongodb
+brew install mysql
+brew install neovim
+brew install nvm
+brew install optipng
+brew install pdf2htmlex
+brew install pngquant
+brew install poppler
+brew install python3
+brew install re2c
+brew install reattach-to-user-namespace
+brew install rename
+brew install homebrew/php/robo
+brew install sbt
+brew install telnet
+brew install homebrew/php/terminus
+brew install the_silver_searcher
+brew install tmux
+brew install tree
+brew install utf8proc
+brew install vim
+brew install homebrew/php/wp-cli
+brew install yarn
+brew install zlib
+brew install zsh
+
+# Remove outdated versions from the cellar.
+brew cleanup
