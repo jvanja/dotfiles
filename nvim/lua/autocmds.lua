a987f06b06bd2fe5b5204f93611b34bdeb905eeb
Vanja Jelic <1101520+jvanja@users.noreply.github.com>
2021-08-26 11:08:33 +0200

    added indent guides colors

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -3,7 +3,7 @@ vim.cmd('au BufReadPost *.item set syntax=html')
 vim.cmd('au BufRead,BufNewFile *.ejs set filetype=html')
 
 -- Automatic reloading of .vimrc
-vim.cmd('autocmd! bufwritepost .vimrc source %')
+-- vim.cmd('autocmd! bufwritepost .vimrc source %')
 
 -- temp file for crontab
 vim.cmd('autocmd filetype crontab setlocal nobackup nowritebackup')
@@ -15,5 +15,5 @@ vim.cmd('runtime macros/matchit.vim')
 -- Vue.js syntax
 -- vim.cmd('autocmd BufRead,BufNewFile *.vue setlocal filetype=vue')
 
--- vim.cmd('autocmd VimEnter,Colorscheme * :hi IndentGuidesOdd  guibg=#444444   ctermbg=3')
--- vim.cmd('autocmd VimEnter,Colorscheme * :hi IndentGuidesEven guibg=#353535 ctermbg=4')
+vim.cmd('autocmd VimEnter,Colorscheme * :hi IndentGuidesOdd  guibg=#444444   ctermbg=3')
+vim.cmd('autocmd VimEnter,Colorscheme * :hi IndentGuidesEven guibg=#353535 ctermbg=4')
