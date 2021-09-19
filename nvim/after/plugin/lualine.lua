bf518c6702f5730678f2303c141cb91bcfd8d6dd
Vanja Jelic <1101520+jvanja@users.noreply.github.com>
2021-09-19 10:04:56 +0200

    added lsp server name to lualine section

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -1,10 +1,26 @@
-require'lualine'.setup {
+local lualine = require 'lualine'
+
+local colors = {
+  bg       = '#202328',
+  fg       = '#bbc2cf',
+  yellow   = '#ECBE7B',
+  cyan     = '#008080',
+  darkblue = '#081633',
+  green    = '#98be65',
+  orange   = '#FF8800',
+  violet   = '#a9a1e1',
+  magenta  = '#c678dd',
+  blue     = '#51afef',
+  red      = '#ec5f67',
+}
+
+local config = {
   options = {
     icons_enabled = true,
     -- theme = 'gruvbox',
     theme = 'onedark',
-    component_separators = {'', ''},
-    section_separators = {'', ''},
+    component_separators = {left = '', right = ''},
+    section_separators = {left = '', right = ''},
     disabled_filetypes = {}
   },
   sections = {
@@ -16,12 +32,13 @@ require'lualine'.setup {
     },
     lualine_c = {'diff'},
     -- lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_x = {
-      'diagnostics',
-      'treesitter',
-      'lsp',
-      'filetype',
-    },
+    lualine_x = {},
+    -- lualine_x = {
+    --   'diagnostics',
+    --   'treesitter',
+    --   'lsp',
+    --   'filetype'
+    -- },
     lualine_y = {'progress'},
     lualine_z = {'location'}
   },
@@ -29,10 +46,56 @@ require'lualine'.setup {
     lualine_a = {},
     lualine_b = {},
     lualine_c = {'filename'},
-    lualine_x = {'location'},
+    -- lualine_x = {'location'},
+    lualine_x = {},
     lualine_y = {},
     lualine_z = {}
   },
   tabline = {},
   extensions = {}
 }
+
+-- Inserts a component in lualine_c at left section
+local function ins_left(component)
+  table.insert(config.sections.lualine_c, component)
+end
+
+-- Inserts a component in lualine_x ot right section
+local function ins_right(component)
+  table.insert(config.sections.lualine_x, component)
+end
+
+ins_right {
+  'diagnostics',
+  sources = { 'nvim_lsp' },
+  symbols = { error = ' ', warn = ' ', info = ' ' },
+  diagnostics_color = {
+    color_error = { fg = colors.red },
+    color_warn = { fg = colors.yellow },
+    color_info = { fg = colors.cyan },
+  },
+}
+
+ins_right {
+  -- Lsp server name .
+  function()
+    local msg = 'No Active Lsp'
+    local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+    local clients = vim.lsp.get_active_clients()
+    if next(clients) == nil then
+      return msg
+    end
+    for _, client in ipairs(clients) do
+      local filetypes = client.config.filetypes
+      if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+        return client.name
+      end
+    end
+    return msg
+  end,
+  icon = ' LSP:',
+  color = { fg = '#ffffff', gui = 'bold' },
+}
+
+lualine.setup(config)
+
