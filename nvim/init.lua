6e8ca162233a2df8db96623ba4940e5141fb00bd
Vanja Jelic <1101520+jvanja@users.noreply.github.com>
2021-10-29 12:18:10 +0200

    removed plugins conf

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,29 +3,20 @@ vim.g.snips_author = "vanja"
 vim.g.snips_email = "jelicvanja@gmail.com"
 vim.g.snips_github = "https://github.com/jvanja"
 
--- let vim know to use BREW version of pythons
+-- let vim know to use BREW version of python
 vim.g.python3_host_prog = '/usr/local/bin/python3'
-
 
 -- set leader key to ,
 vim.g.mapleader = ","
 
 -- OPTIONS
--- vim.cmd('source ~/.config/nvim/lua/options.lua')
 require('options')
 
 -- PLUGINS
--- vim.cmd('source ~/.config/nvim/lua/plugins/init.lua')
 require('plugins')
-
--- vim.cmd('source ~/.config/nvim/lua/plugins/plugins_conf.lua')
-require('plugins.plugins_conf')
--- require ( "plugins.configs.bufferline" )
-
 
 -- THEME
 require('theme')
-
 
 -- AUTO COMMANDS
 require('autocmds')
