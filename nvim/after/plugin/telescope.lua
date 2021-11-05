a1c6130d3b460670d4fabdd0d08a57c23d3f0b76
Vanja Jelic <1101520+jvanja@users.noreply.github.com>
2021-11-05 15:12:36 +0100

    LOVE TELESCOPE!!!!

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -87,4 +87,5 @@ vim.api.nvim_set_keymap("n", "<leader>fg", "<cmd>lua require('telescope.builtin'
 vim.api.nvim_set_keymap("n", "<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<CR>", opts)
 vim.api.nvim_set_keymap("n", "<leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<CR>", opts)
 vim.api.nvim_set_keymap("n", "<leader>?", "<cmd>lua require('telescope.builtin').keymaps()<CR>", opts)
+vim.api.nvim_set_keymap("n", "<leader>fc", "<cmd>lua require('telescope.builtin').git_bcommits()<CR>", opts)
 
