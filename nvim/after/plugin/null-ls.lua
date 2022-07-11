b01dfddd77fae911693f29e9e272bb24368656b3
Vanja Jelic <1101520+jvanja@users.noreply.github.com>
2022-07-11 10:28:14 +0200

    extracted configs to a config file

--- nvim/after/plugin/null-ls.lua
+++ nvim/after/plugin/null-ls.lua
@@ -11,15 +11,17 @@ local diagnostics = null_ls.builtins.diagnostics
 -- function M.setup(opts)
   null_ls.setup({
     sources = {
-      formatting.prettierd,
-      -- formatting.eslint.with({
-      --   filetypes = { 'javascript' }
-      -- }),
+      -- formatting.prettierd,
+      formatting.eslint.with({
+        filetypes = { 'javascript' }
+      }),
+      formatting.eslint_d,
       diagnostics.eslint.with({
         filetypes = { 'vue', 'javascript' },
         diagnostics_format = '[#{c}] #{m} (#{s})'
       }),
     },
+    on_attach = require("core.config").on_attach
   })
 -- end
 
