06b3bb9f14202680511b65e06ace2d7d44743694
Vanja Jelic <1101520+jvanja@users.noreply.github.com>
2022-10-02 08:39:34 +0200

    added lsp restart map

--- nvim/lua/core/config.lua
+++ nvim/lua/core/config.lua
@@ -22,6 +22,7 @@ M.on_attach = function(client, bufnr)
   buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
   buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
   buf_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  buf_set_keymap('n', '<leader>lr', ':LspRestart<CR>', opts)
   -- use telescope for below mappings
   -- buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
   -- buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
