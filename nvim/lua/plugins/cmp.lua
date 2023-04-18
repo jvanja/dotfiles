e59c6326ae98fc56e9bcd3650c091badb9f918f3
Vanja Jelic <1101520+jvanja@users.noreply.github.com>
2023-04-18 10:20:05 +0200

    added codeium to kids

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -6,5 +6,15 @@ return {
   opts = function(_, opts)
     local cmp = require("cmp")
     opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "codeium" } }))
+    opts.formatting = {
+      format = function(_, item)
+        local icons = require("lazyvim.config").icons.kinds
+        if icons[item.kind] then
+          icons["Codeium"] = " "
+          item.kind = icons[item.kind] .. item.kind
+        end
+        return item
+      end,
+    }
   end,
 }
