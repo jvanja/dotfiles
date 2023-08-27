580314178859f5c015b40b7010c7176dc1476a13
Vanja Jelic <1101520+jvanja@users.noreply.github.com>
2023-08-27 21:20:03 +0200

    removed bufremove

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,5 +1,6 @@
 return {
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
-  { "echasnovski/mini.surround", enabled = false },
-  { "folke/flash.nvim", enabled = false },
+  { "echasnovski/mini.surround",   enabled = false },
+  { "echasnovski/mini.bufremove",  enabled = false },
+  { "folke/flash.nvim",            enabled = false },
 }
