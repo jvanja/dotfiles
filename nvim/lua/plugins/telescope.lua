956107d002e685d97c3ac991736227a2cc57afa9
Vanja Jelic <1101520+jvanja@users.noreply.github.com>
2023-08-29 12:53:51 +0200

    added telescope-live-grep-args.nvim with args

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,8 +1,33 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
-    opts = {
-      defaults = {
+    dependencies = {
+      {
+        "nvim-telescope/telescope-file-browser.nvim",
+        config = function()
+          require("telescope").load_extension("file_browser")
+        end,
+      },
+      {
+        "nvim-telescope/telescope-live-grep-args.nvim",
+        version = "^1.0.0",
+      },
+    },
+    opts = function(_, opts)
+      local lga_actions = require("telescope-live-grep-args.actions")
+
+      opts.extensions = {
+        live_grep_args = {
+          auto_quoting = true,
+          mappings = {
+            i = {
+              ["<C-k>"] = lga_actions.quote_prompt(),
+              ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+            },
+          },
+        }
+      }
+      opts.defaults = vim.tbl_deep_extend("force", opts.defaults or {}, {
         selection_strategy = "reset",
         sorting_strategy = "descending",
         layout_strategy = "horizontal",
@@ -23,31 +48,49 @@ return {
           "node_modules",
           "build",
         },
-      },
-    },
-    dependencies = {
-      {
-        "nvim-telescope/telescope-file-browser.nvim",
-        config = function()
-          require("telescope").load_extension("file_browser")
-        end,
-      },
-    },
+      })
+    end,
     keys = {
-      { "<leader>o",  "<cmd>lua require('telescope.builtin').find_files()<CR>",      desc = "Find files" },
-      { "<leader>ff", ":Telescope file_browser<CR>",                                 desc = "File Browser" },
-      { '<leader>"',  "<cmd>lua require('telescope.builtin').registers()<CR>",       desc = "Registers" },
-      { "<leader>fd", "<cmd>lua require('telescope.builtin').lsp_definitions()<CR>", desc = "LSP Definitions" },
-      { "<leader>fr", "<cmd>lua require('telescope.builtin').lsp_references()<CR>",  desc = "LSP References" },
-      { "<leader>fc", "<cmd>lua require('telescope.builtin').git_bcommits()<CR>",    desc = "Search in buffer commits" },
-      { "<leader>fs", "<cmd>lua require('telescope.builtin').git_status()<CR>" },
+      { "<leader>o", "<cmd>lua require('telescope.builtin').find_files()<CR>", desc = "Find files" },
+      {
+        "<leader>ff",
+        ":Telescope file_browser<CR>",
+        desc =
+        "File Browser"
+      },
+      { '<leader>"', "<cmd>lua require('telescope.builtin').registers()<CR>",  desc = "Registers" },
+      {
+        "<leader>fd",
+        "<cmd>lua require('telescope.builtin').lsp_definitions()<CR>",
+        desc =
+        "LSP Definitions"
+      },
+      {
+        "<leader>fr",
+        "<cmd>lua require('telescope.builtin').lsp_references()<CR>",
+        desc =
+        "LSP References"
+      },
+      {
+        "<leader>fc",
+        "<cmd>lua require('telescope.builtin').git_bcommits()<CR>",
+        desc =
+        "Search in buffer commits"
+      },
+      { "<leader>fs", "<cmd>lua require('telescope.builtin').git_status()<CR>",                       desc = "Git status" },
+      { "<leader>sg", "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", desc = "Git status" },
       {
         "<leader>?",
         "<cmd>lua require('telescope.builtin').keymaps()<CR>",
         desc =
         "Lists normal mode keymappings"
       },
-      { "<leader>fw", "<cmd>lua require('telescope.builtin').grep_string()<CR>", desc = "Grep for word under cursor" },
+      {
+        "<leader>fw",
+        "<cmd>lua require('telescope.builtin').grep_string()<CR>",
+        desc =
+        "Grep for word under cursor"
+      },
       { "<leader>gs", false },
     },
   },
