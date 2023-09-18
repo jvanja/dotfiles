65cf89e6d69b04edd0eb3ef0cef89d277560ad59
Vanja Jelic <1101520+jvanja@users.noreply.github.com>
2023-09-18 14:36:13 +0200

    added some fancy colored output

--- install.sh
+++ install.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 
+# Color variables
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+blue='\033[0;34m'
+magenta='\033[0;35m'
+cyan='\033[0;36m'
+bg_red='\033[0;41m'
+bg_green='\033[0;42m'
+bg_yellow='\033[0;43m'
+bg_blue='\033[0;44m'
+bg_magenta='\033[0;45m'
+bg_cyan='\033[0;46m'
+# Clear the color after that
+clear='\033[0m'
+
 CUR_PATH=$(pwd)
 
-echo "creating dotfiles..."
+echo -e "---------------------------------------"
+echo -e "Creating dotfiles..."
+echo -e "---------------------------------------"
 linkables=$( ls -1 -d *.symlink )
 for file in $linkables ; do
 	target="$HOME/.$( basename $file ".symlink" )"
-	echo "copying $CUR_PATH/$file to $target"
+	echo -e "${cyan}copying${clear} $CUR_PATH/$file to $target"
 	cp $CUR_PATH/$file $target
 done
 
-echo "syncing nvim config"
-rsync -a $CUR_PATH/nvim/ ~/.config/nvim
+echo -e "..................."
+echo -e "${yellow}Syncing nvim config${clear}"
+echo -e "..................."
+rsync -avh $CUR_PATH/nvim/ ~/.config/nvim --delete
