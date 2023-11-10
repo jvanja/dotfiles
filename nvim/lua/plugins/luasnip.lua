9af356936b7979a77c4dab7be58e86d4adb5cba4
Vanja Jelic <1101520+jvanja@users.noreply.github.com>
2023-11-10 14:22:11 +0100

    added vscode style custom snippets loading

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -5,6 +5,7 @@ return {
     delete_check_events = "TextChanged",
   },
   config = function()
-    require("luasnip.loaders.from_lua").load({ paths = "./snippets" })
+    require("luasnip.loaders.from_lua").load({ paths = "./snippets" }) -- for anything dynamic
+    require("luasnip.loaders.from_vscode").lazy_load({ paths = "./snippets" }) -- simple snippets in json files
   end,
 }
