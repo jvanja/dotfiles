535a0ce02b764b628bcbd51674d9ad599798667a
Vanja Jelic <vanjajelic@Vanjas-Mac-mini.local>
2024-05-27 10:16:26 +0200

    replaced codeium with supermaven

---
+++ nvim/lua/plugins/ai.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "Exafunction/codeium.nvim",
+    event = "VeryLazy",
+    enabled = false,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "hrsh7th/nvim-cmp",
+    },
+    config = function()
+      require("codeium").setup({})
+    end,
+  },
+  {
+    "supermaven-inc/supermaven-nvim",
+    config = function()
+      require("supermaven-nvim").setup({})
+    end,
+  },
+}
