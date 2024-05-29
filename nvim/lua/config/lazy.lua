76b74bbd90bb8e7387aaaa761e4cb9beb6ac97b4
Vanja Jelic <vanjajelic@Vanjas-Mac-mini.local>
2024-05-29 10:54:41 +0200

    updated starter lazy

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -1,9 +1,9 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
   -- bootstrap lazy.nvim
   -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
-    lazypath })
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
@@ -38,7 +38,6 @@ require("lazy").setup({
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   -- install = { colorscheme = { "tokyonight", "habamax" } },
-  -- install = { colorscheme = { "monokai-pro" } },
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
