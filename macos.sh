cd3ec93eb7a251b9e6e9802860a4d8c2ef05d5ce
Vanja Jelic <1101520+jvanja@users.noreply.github.com>
2024-06-18 14:28:39 +0200

    added show hidden files

--- macos.sh
+++ macos.sh
@@ -1,3 +1,6 @@
+# show hidden files
+defaults write com.apple.Finder AppleShowAllFiles true
+
 # Set a blazingly fast keyboard repeat rate
 defaults write NSGlobalDomain KeyRepeat -int 1 # normal minimum is 2 (30 ms)
 defaults write NSGlobalDomain InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
