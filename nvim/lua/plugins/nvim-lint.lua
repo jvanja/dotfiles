957484da6de9d09884b576f81226aa2410df0ee3
Vanja Jelic <1101520+jvanja@users.noreply.github.com>
2024-07-02 16:43:39 +0200

    format

--- nvim/lua/plugins/nvim-lint.lua
+++ nvim/lua/plugins/nvim-lint.lua
@@ -2,10 +2,10 @@ return {
   "mfussenegger/nvim-lint",
   opts = {
     linters_by_ft = {
-      vue = {'eslint',},
-      javascript = {'eslint',},
-      typescript = {'eslint',},
-      htmldjango = {'djlint',}
-    }
-  }
+      vue = { "eslint" },
+      javascript = { "eslint" },
+      typescript = { "eslint" },
+      htmldjango = { "djlint" },
+    },
+  },
 }
