4bac6e35cfd704c8c03ab9f6e7094ad5534e4e44
Vanja Jelic <1101520+jvanja@users.noreply.github.com>
2024-07-03 15:13:03 +0200

    added nvim-lint fix for eslint not working with eslint.config.js

--- nvim/lua/plugins/nvim-lint.lua
+++ nvim/lua/plugins/nvim-lint.lua
@@ -1,11 +1,41 @@
 return {
   "mfussenegger/nvim-lint",
-  opts = {
-    linters_by_ft = {
+  -- opts = {
+  --   linters_by_ft = {
+  --     vue = { "eslint" },
+  --     javascript = { "eslint" },
+  --     typescript = { "eslint" },
+  --     htmldjango = { "djlint" },
+  --   },
+  -- },
+  event = { 'BufReadPre', 'BufNewFile' },
+  config = function()
+    local lint = require("lint")
+    lint.linters_by_ft = {
       vue = { "eslint" },
       javascript = { "eslint" },
+      javascriptreact = { "eslint" },
       typescript = { "eslint" },
+      typescriptreact = { "eslint" },
       htmldjango = { "djlint" },
-    },
-  },
+    }
+
+    -- fixes this https://github.com/mfussenegger/nvim-lint/issues/610
+    local eslint = require("lint.linters.eslint")
+    eslint.args = vim.tbl_extend("force", {
+      "--config",
+      function()
+        return vim.fn.getcwd() .. "/eslint.config.js"
+      end,
+    }, eslint.args)
+
+    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+      group = lint_augroup,
+      callback = function()
+        lint.try_lint()
+      end,
+    })
+  end,
 }
