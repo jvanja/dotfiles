917678f12a2ad0e2e21aa570b3ce95cfa6ee16a3
Vanja Jelic <1101520+jvanja@users.noreply.github.com>
2025-04-11 10:08:17 +0200

    re-enabled noice

--- bash/config/nvim/lua/plugins/disabled.lua
+++ bash/config/nvim/lua/plugins/disabled.lua
@@ -1,5 +1,5 @@
 return {
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   -- { "echasnovski/mini.surround",   enabled = false },
-  { "folke/noice.nvim",   enabled = false },
+  -- { "folke/noice.nvim",   enabled = false },
 }
