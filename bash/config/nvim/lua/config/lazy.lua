b08d28ef3198af65dca26fd85ab18d68de160916
Vanja Jelic <1101520+jvanja@users.noreply.github.com>
2025-07-28 14:01:35 +0200

    removed extra line

--- bash/config/nvim/lua/config/lazy.lua
+++ bash/config/nvim/lua/config/lazy.lua
@@ -19,7 +19,6 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
 
-
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.vue" },
