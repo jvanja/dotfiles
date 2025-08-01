29d582308fe5c57d3b3ab5a493e96abd12c3c116
Vanja Jelic <1101520+jvanja@users.noreply.github.com>
2025-08-01 16:01:13 +0200

    removed unneccessary config

--- bash/config/nvim/lua/plugins/blink-cmp.lua
+++ bash/config/nvim/lua/plugins/blink-cmp.lua
@@ -6,10 +6,6 @@ return {
 
     opts = {
       keymap = { preset = "default" },
-      appearance = {
-        use_nvim_cmp_as_default = true,
-        nerd_font_variant = "mono",
-      },
 
       sources = {
         default = { "supermaven", "snippets", "lsp", "path", "buffer" },
