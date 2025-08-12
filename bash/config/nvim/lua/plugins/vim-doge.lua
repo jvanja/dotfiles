8aea8632bab3ecebe7a70919af414dab7ad00e2a
Vanja Jelic <1101520+jvanja@users.noreply.github.com>
2025-08-12 09:24:53 +0200

    changed keymap

--- bash/config/nvim/lua/plugins/vim-doge.lua
+++ bash/config/nvim/lua/plugins/vim-doge.lua
@@ -2,6 +2,6 @@ return {
   "kkoomen/vim-doge",
   config = function()
     -- vim.cmd([[call doge#install()]])
-    vim.keymap.set("n", "<Leader>d", "<Plug>(doge-generate)", { desc = "Generate docs (vim-doge)" })
+    vim.keymap.set("n", "<Leader>gd", "<Plug>(doge-generate)", { desc = "Generate docs (vim-doge)" })
   end,
 }
