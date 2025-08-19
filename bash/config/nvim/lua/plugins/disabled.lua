872906a54e685dfb9f98d4c8ac4ecebc09013dc6
Vanja Jelic <1101520+jvanja@users.noreply.github.com>
2025-08-19 09:35:49 +0200

    removed catpuccin

--- bash/config/nvim/lua/plugins/disabled.lua
+++ bash/config/nvim/lua/plugins/disabled.lua
@@ -1,5 +1,6 @@
 return {
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "catppuccin/nvim",   enabled = false },
   -- { "echasnovski/mini.surround",   enabled = false },
   -- { "folke/noice.nvim",   enabled = false },
 }
