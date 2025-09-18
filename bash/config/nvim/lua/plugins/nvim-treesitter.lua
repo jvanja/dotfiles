c3131816a3500216fd9dfa6726370ec7aafa9568
Vanja Jelic <1101520+jvanja@users.noreply.github.com>
2025-09-18 17:26:44 +0200

    updated treesitter config for lazyvim 15

--- bash/config/nvim/lua/plugins/nvim-treesitter.lua
+++ bash/config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,42 +1,40 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-      { "windwp/nvim-ts-autotag" },
-    },
-    opts = function(_, opts)
-      ---@class ParserInfo[]
-      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-      parser_config.blade = {
-        install_info = {
-          url = "https://github.com/EmranMR/tree-sitter-blade",
-          files = { "src/parser.c" },
-          branch = "main",
-        },
-        filetype = "blade",
-      }
-
-      vim.list_extend(opts.ensure_installed, {
-        "vue",
-        "php",
-        "scss",
-        "css",
-        "bash",
-        "regex",
-        "markdown",
-      })
-      opts.autotag = {
-        enable = true,
-      }
-      opts.incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<CR>",
-          node_incremental = "<CR>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      }
-    end,
+  "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    { "windwp/nvim-ts-autotag" },
   },
+  opts = function(_, opts)
+    -- Add blade parser configuration
+    opts.parsers = opts.parsers or {}
+    opts.parsers.blade = {
+      install_info = {
+        url = "https://github.com/EmranMR/tree-sitter-blade",
+        files = { "src/parser.c" },
+        branch = "main",
+      },
+      filetype = "blade",
+    }
+
+    vim.list_extend(opts.ensure_installed, {
+      "vue",
+      "php",
+      "scss",
+      "css",
+      "bash",
+      "regex",
+      "markdown",
+    })
+    opts.autotag = {
+      enable = true,
+    }
+    opts.incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<CR>",
+        node_incremental = "<CR>",
+        scope_incremental = false,
+        node_decremental = "<bs>",
+      },
+    }
+  end,
 }
