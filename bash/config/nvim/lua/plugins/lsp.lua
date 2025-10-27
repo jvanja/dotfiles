c7142b3149545e1d0dbde8d927446932076d238a
Vanja Jelic <1101520+jvanja@users.noreply.github.com>
2025-10-27 09:31:51 +0100

    simplified LSP config

--- bash/config/nvim/lua/plugins/lsp.lua
+++ bash/config/nvim/lua/plugins/lsp.lua
@@ -29,86 +29,75 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
-    opts = function()
-      -- local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      -- keys[#keys + 1] = { ",cl", false }
-      local ret = {
-        diagnostics = {},
-        codelens = {},
-        document_highlight = { enabled = false },
-        folds = {},
-        inlay_hints = {
-          enabled = false,
+    opts = {
+      document_highlight = { enabled = false },
+      inlay_hints = { enabled = false },
+      format = {
+        formatting_options = nil,
+        timeout_ms = 3000,
+      },
+      servers = {
+        intelephense = {
+          settings = require("config.intelephense_conf"),
         },
-        format = {
-          formatting_options = nil,
-          timeout_ms = 3000,
+        phpactor = {
+          init_options = {
+            ["language_server.diagnostics_on_open"] = false,
+            ["language_server.diagnostics_on_update"] = false,
+            ["language_server.diagnostics_on_save"] = false,
+          },
         },
-        servers = {
-          intelephense = {
-            settings = require("config.intelephense_conf"),
-          },
-          phpactor = {
-            init_options = {
-              ["language_server.diagnostics_on_open"] = false,
-              ["language_server.diagnostics_on_update"] = false,
-              ["language_server.diagnostics_on_save"] = false,
-            },
-          },
-          vtsls = {
-            filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
-            settings = {
-              vtsls = {
-                tsserver = {
-                  globalPlugins = {
-                    {
-                      name = "@vue/typescript-plugin",
-                      location = LazyVim.get_pkg_path("vue-language-server", "/node_modules/@vue/language-server"),
-                      languages = { "vue" },
-                      configNamespace = "typescript",
-                      enableForWorkspaceTypeScriptVersions = true,
-                    },
+        vtsls = {
+          filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+          settings = {
+            vtsls = {
+              tsserver = {
+                globalPlugins = {
+                  {
+                    name = "@vue/typescript-plugin",
+                    location = LazyVim.get_pkg_path("vue-language-server", "/node_modules/@vue/language-server"),
+                    languages = { "vue" },
+                    configNamespace = "typescript",
+                    enableForWorkspaceTypeScriptVersions = true,
                   },
                 },
               },
             },
           },
-          emmet_language_server = {
-            filetypes = {
-              "css",
-              "scss",
-              "eruby",
-              "html",
-              "htmldjango",
-              "blade",
-              "php",
-              "javascript",
-              "javascriptreact",
-              "less",
-              "sass",
-              "scss",
-              "svelte",
-              "pug",
-              "typescriptreact",
-              "vue",
-            },
+        },
+        emmet_language_server = {
+          filetypes = {
+            "css",
+            "scss",
+            "eruby",
+            "html",
+            "htmldjango",
+            "blade",
+            "php",
+            "javascript",
+            "javascriptreact",
+            "less",
+            "sass",
+            "scss",
+            "svelte",
+            "pug",
+            "typescriptreact",
+            "vue",
           },
-          lua_ls = {
-            settings = {
-              Lua = {
-                diagnostics = {
-                  globals = { "vim" },
-                },
+        },
+        lua_ls = {
+          settings = {
+            Lua = {
+              diagnostics = {
+                globals = { "vim" },
               },
             },
           },
-          tailwindcss = {
-            filetypes_include = { "php" },
-          },
         },
-        setup = {},
-      }
-      return ret
-    end,
+        tailwindcss = {
+          filetypes_include = { "php" },
+        },
+      },
+    },
   },
 }
